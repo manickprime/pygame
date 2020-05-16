96ea9d07032001a16e4072b994a3119170bf72b9
manick <manickprime@gmail.com>
2020-05-16 19:15:52 +0530

    added auto-motion in slither game.
    now the snake moves without having to press the arrow key,the user only have to change the direction of it's movement.

--- slither/new.py
+++ slither/new.py
@@ -1,8 +1,10 @@
 import pygame
-import sys,random,time
+import random,time
 
 pygame.init()
 
+
+#setting the screen
 screen = pygame.display.set_mode((720,460))
 pygame.display.set_caption('snake game')
 
@@ -11,18 +13,13 @@ red = pygame.Color(255, 0, 0) # gameover
 green = pygame.Color(0, 255, 0) #snake
 black = pygame.Color(0, 0, 0) #score
 white = pygame.Color(255, 255, 255) #background
-brown = pygame.Color(165, 42, 42) #food
 
 running = True
 
-
-#fpsController = pygame.time.Clock()
-
-
 #snake's positions
 snakePos = [100,50]
-snakeBody=[[100,50]]#,[90,50],[80,50]]
-initial=[[100,50]]#,[90,50],[80,50]]
+snakeBody=[[100,50]]
+initial=[[100,50]]
 
 
 #apple's position
@@ -34,12 +31,20 @@ direction = 'RIGHT'
 changeto = direction
 
 score_value = 0
+high_score = 0
 font = pygame.font.Font('freesansbold.ttf',20)
-gameExit = False
+highScoreFont = pygame.font.Font('freesansbold.ttf',12)
+
+clock = pygame.time.Clock()
 
 def show_score():
-    score = font.render("Score:"+str(score_value),True,black   )
+    score = font.render("score:"+str(score_value),True,black)
     screen.blit(score,(10,10))
+
+#optional
+def show_hscore():
+    score = highScoreFont.render("high score:"+str(high_score),True,black)
+    screen.blit(score,(10,35))#adding font size to the score's x coordinate
 
 def restart():
     snakeBody.clear()
@@ -53,9 +58,12 @@ def restart():
 while running:
     grow=0
     screen.fill(white)
+    
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
-            running = False
+            running = False        
+
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 changeto = 'RIGHT'
@@ -64,11 +72,7 @@ while running:
             if event.key == pygame.K_UP:
                 changeto = 'UP'
             if event.key == pygame.K_DOWN:
-                changeto = 'DOWN'
-            
-
-
-
+                changeto = 'DOWN'              
 
         if changeto == 'RIGHT' and not direction == 'LEFT':
             direction = 'RIGHT'
@@ -79,55 +83,60 @@ while running:
         if changeto == 'DOWN' and not direction == 'UP':
             direction = 'DOWN'
 
-        if direction == 'RIGHT':
-            snakePos[0] += 10
-        if direction == 'LEFT':
-            snakePos[0] -= 10
-        if direction == 'UP':
-            snakePos[1] -= 10
-        if direction == 'DOWN':
-            snakePos[1] += 10
+    if direction == 'RIGHT':
+        snakePos[0] += 10
+    if direction == 'LEFT':
+        snakePos[0] -= 10
+    if direction == 'UP':
+        snakePos[1] -= 10
+    if direction == 'DOWN':
+        snakePos[1] += 10
 
 
-        snakeBody.insert(0,list(snakePos))
-        if snakePos[0] == foodPos[0] and snakePos[1] == foodPos[1]:
-            foodSpawn = False
-            score_value+=1
-        else:
-            snakeBody.pop()
+    snakeBody.insert(0,list(snakePos))
+    if snakePos[0] == foodPos[0] and snakePos[1] == foodPos[1]:
+        foodSpawn = False
+        score_value+=1
+    else:
+        snakeBody.pop()
 
 
-        if foodSpawn == False:
-            foodPos = [random.randrange(1,72)*10,random.randrange(1,46)*10]
-            foodSpawn = True
+    #generating new food
+    if foodSpawn == False:
+        foodPos = [random.randrange(1,72)*10,random.randrange(1,46)*10]
+        foodSpawn = True
 
-        #checking it's not eating itself
-        for block in snakeBody[1:]:
-            if snakePos[0] == block[0] and snakePos[1] == block[1]:
-                restart()
-                score_value=0
-
-        #checking if it stays in the screen
-        if snakePos[0] > 710 or snakePos[0] < 0:
+    #checking it's not eating itself
+    for block in snakeBody[1:]:
+        if snakePos[0] == block[0] and snakePos[1] == block[1]:
             restart()
+            direction = 'RIGHT'
+            high_score = score_value
             score_value=0
+
+    #checking if it stays in the screen
+    if snakePos[0] > 710 or snakePos[0] < 0:
+        restart()
+        direction = 'RIGHT'
+        score_value=0
             
-        if snakePos[1] > 450 or snakePos[1] < 0:
-            restart()
-            score_value=0
-
-                
-
-    
+    if snakePos[1] > 450 or snakePos[1] < 0:
+        restart()
+        direction = 'RIGHT'
+        score_value=0             
 
 
     #displaying the snake
     for pos in snakeBody:
         pygame.draw.rect(screen,red,pygame.Rect(pos[0],pos[1],10,10))
 
-    pygame.draw.rect(screen, brown, pygame.Rect(foodPos[0],foodPos[1],10,10))
+    #drawing the snake in the screen
+    pygame.draw.rect(screen, green, pygame.Rect(foodPos[0],foodPos[1],10,10))
     show_score()
+    if score_value>=high_score:
+        high_score=score_value
+    show_hscore()
     pygame.display.update()
-
-    
-    
+    #choose the speed of the snake
+    #larger number = more speed
+    clock.tick(15)
